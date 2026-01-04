8a500ee3008a2a2f73dfed1ad48a0de373dc57da
Geoffrey Silva <geoff@loweffortmedia.com>
2026-01-03 19:16:33 -0500

    Initial Commit

---
+++ Elapse(D) but for the Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elapse(D) but for the Mac
+//
+//  Created by Geoffrey Silva on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elapse(D) but for the Mac/Elapse_D__but_for_the_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elapse_D__but_for_the_MacApp.swift
+//  Elapse(D) but for the Mac
+//
+//  Created by Geoffrey Silva on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Elapse_D__but_for_the_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
