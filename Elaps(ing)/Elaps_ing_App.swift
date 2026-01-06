9294e750194796b67cef5e8c8ca9b3d1dff1df7c
Geoffrey Silva <geoff@loweffortmedia.com>
2026-01-06 04:09:11 -0500

    Initial Commit

---
+++ Elaps(ing)/Elaps_ing_App.swift
@@ -0,0 +1,17 @@
+//
+//  Elaps_ing_App.swift
+//  Elaps(ing)
+//
+//  Created by Geoffrey Silva on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Elaps_ing_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
